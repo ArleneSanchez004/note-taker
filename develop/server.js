d2d6a33a2c7f243b3416c25923ac22014811f6d1
Arlene Sanchez <arlene.sanchez004@gmail.com>
2020-05-20 16:04:40 -0400

    server.js initial setup

---
+++ develop/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded ({ extended: true }));
+app.use(express.json());
+//app.use(express.static("public"));
+
+app.get("*", (req, res) => res.send("hello"));
+
+app.listen(PORT, () => console.log(`App listening on http://localhost:${PORT}`));
+
